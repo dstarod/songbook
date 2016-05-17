88ffdab5509fca4649ae6afad19b564323ddc14e
DMITRY STARODUBTSEV <dmitry.starodubcev@gmail.com>
2016-05-17 16:30:46 +0300

    Make font smaller

--- chords/views.py
+++ chords/views.py
@@ -279,7 +279,7 @@ def make_pdf(songs):
             font_size -= 1
             document_w = width * font_size
 
-        font_size *= 1.2
+        font_size *= 1.1
 
         style_name = 'chords_{}'.format(font_size)
         if style_name not in styles:
